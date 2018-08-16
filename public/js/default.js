a48d62d6e09ede6f1ed48c2f406f14b53060d076
Milan Masnikosa <m.masnikosa@praktikant-001.vega.local>
2018-08-16 14:06:23 +0200

    recompiling .js files

---
+++ public/js/default.js
@@ -0,0 +1,121 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 40);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 40:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(41);
+
+
+/***/ }),
+
+/***/ 41:
+/***/ (function(module, exports) {
+
+$(function () {
+	helper.toggleSubnav();
+	helper.toggleLoginSubnav();
+	helper.toggleBasketSubnav();
+	helper.closeSubnav();
+	helper.searchToggle();
+	helper.navSubmenuHover();
+	helper.languageDropdown();
+	helper.subscribeBlock();
+	helper.tabs();
+	helper.colorSelection();
+	helper.productSize();
+	helper.filtersToggle();
+	helper.fixedFiltersToggle();
+	helper.filtersSelection();
+	helper.resetFilters();
+	helper.filtersPosition();
+	helper.subrowToggle();
+	helper.stickyProducts();
+	helper.googleMaps();
+	helper.productImageZoom();
+	helper.gridToggle();
+	helper.breadcrumbsToggle();
+	helper.productColorSlider();
+	helper.productSizeSlider();
+	helper.discountToggle();
+	helper.thumbPDP();
+	helper.tooltipForm();
+	helper.customLanguageDropdown();
+});
+
+$(window).on('load', function () {});
+
+$(window).resize(function () {});
+
+$(window).on('scroll', function () {
+	helper.filtersPosition();
+	helper.stickyProducts();
+});
+
+/***/ })
+
+/******/ });
